1cd99af61310d4a62c9bbadbd4aab35c65c0444a
mdjunaidalhabib <mdjunaidalhabib20@gmail.com>
2026-07-05 15:06:24 +0600

    Updated code

--- admin/src/app/admin/products/page.jsx
+++ admin/src/app/admin/products/page.jsx
@@ -280,7 +280,7 @@ export default function ProductsPage() {
       {loading ? (
         <ProductsSkeleton />
       ) : filteredProducts.length > 0 ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 2xl:grid-cols-6  gap-4">
           {filteredProducts.map((p) => (
             <ProductCard
               key={p._id}
